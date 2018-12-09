2f51edad9dbcb936fc140033ecdf193965e2a003
UIWEBTIAN <you@example.com>
2018-12-09 15:27:18 +0800

    项目初始化

---
+++ src/pages/mine/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    我是首页
+    <ul>
+      <li @longtap="deleteOne(index)" v-for="(item, index) in list" :key="index">
+        {{ item.name }}--- {{ item.study }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data:function(){
+      return {
+        list:[
+          {name:"一颗牙疼",study:"考研加油"},
+          {name:"添添宝贝猪",study:"Wait for you"},
+        ]
+      }
+    },
+    methods:{
+      deleteOne(index){
+        
+        this.list.splice(index,1)
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
+
